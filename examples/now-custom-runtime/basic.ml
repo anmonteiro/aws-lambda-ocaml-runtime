76b2642751319f7a247d0e1d031999c8872138f6
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2018-12-04 16:21:28 +0000

    add an example of building a lambda for Zeit's Now 2.0 with

---
+++ examples/now-custom-runtime/basic.ml
@@ -0,0 +1,19 @@
+let my_handler evt _context =
+  match Yojson.Safe.Util.member "body" evt with
+  | `String body ->
+    Ok (`Assoc [
+      "statusCode", `Int 200;
+      "body", `String (Yojson.Safe.prettify body);
+      "headers", (`Assoc []);
+    ])
+  | _ -> Error "Body wasn't string"
+
+let setup_log ?style_renderer level =
+  Fmt_tty.setup_std_outputs ?style_renderer ();
+  Logs.set_level level;
+  Logs.set_reporter (Logs_fmt.reporter ());
+  ()
+
+let () =
+  setup_log (Some Logs.Debug);
+  Lambda_runtime.start my_handler
