a74419cd063ca38aa984de1ea25e750fb1f3bf6b
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2018-12-12 02:16:46 +0000

    Add example

---
+++ examples/now-lambda/basic.ml
@@ -0,0 +1,23 @@
+let my_handler { Now_lambda.body; host } _context =
+  let body = match body with
+  | Some body -> body
+  | None -> (Printf.sprintf "Didn't get an HTTP body from %s" host)
+  in
+  Ok Now_lambda.{
+    status_code = 200;
+    headers = Lambda_runtime.StringMap.(empty
+      |> add "content-type" "application/json");
+    body;
+    encoding = None;
+  }
+
+
+let setup_log ?style_renderer level =
+  Fmt_tty.setup_std_outputs ?style_renderer ();
+  Logs.set_level level;
+  Logs.set_reporter (Logs_fmt.reporter ());
+  ()
+
+let () =
+  setup_log (Some Logs.Debug);
+  Now_lambda.lambda my_handler
