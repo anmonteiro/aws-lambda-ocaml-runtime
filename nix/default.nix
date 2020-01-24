a2253a62b37acd137436cbcc03f1ba8f67a18562
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2020-01-23 18:47:55 -0800

    Simplify Nix derivations, gh-actions (#50)

--- nix/default.nix
+++ nix/default.nix
@@ -1,19 +1,37 @@
-{ pkgs ? import <nixpkgs> {}, ocamlVersion ? "4_09" }:
+{ pkgs ? import ./sources.nix { inherit ocamlVersion; }
+, ocamlVersion ? "4_09" }:
 
 let
-  gitignoreSrc = pkgs.fetchFromGitHub {
-    owner = "hercules-ci";
-    repo = "gitignore";
-    rev = "7415c4f";
-    sha256 = "1zd1ylgkndbb5szji32ivfhwh04mr1sbgrnvbrqpmfb67g2g3r9i";
-  };
-  inherit (import gitignoreSrc { inherit (pkgs) lib; }) gitignoreSource;
-  overlays = builtins.fetchTarball https://github.com/anmonteiro/nix-overlays/archive/master.tar.gz;
-
-  ocamlPackages = pkgs.ocaml-ng."ocamlPackages_${ocamlVersion}".overrideScope'
-    (pkgs.callPackage "${overlays}/ocaml" { });
+  inherit (pkgs) lib stdenv ocamlPackages;
 in
-  pkgs.callPackage ./generic.nix {
-    inherit ocamlPackages gitignoreSource;
-  }
 
+  with ocamlPackages;
+  let
+    build-lambda-runtime = args: buildDunePackage ({
+      version = "0.1.0-dev";
+      src = lib.gitignoreSource ./..;
+    } // args);
+
+  in rec {
+    lambda-runtime = build-lambda-runtime {
+      pname = "lambda-runtime";
+      buildInputs = [ alcotest ];
+      doCheck = false;
+      propagatedBuildInputs = [ yojson ppx_deriving_yojson piaf uri logs lwt4 ];
+    };
+
+    now = build-lambda-runtime {
+      pname = "now";
+      buildInputs = [ alcotest ];
+      # tests lambda-runtime too
+      doCheck = true;
+      propagatedBuildInputs = [
+        lambda-runtime
+        httpaf
+        yojson
+        ppx_deriving_yojson
+        lwt4
+        base64
+      ];
+    };
+}
---
+++ nix/sources.nix
@@ -0,0 +1,20 @@
+{ ocamlVersion ? "4_09" }:
+
+let
+  overlays = builtins.fetchTarball {
+    url = https://github.com/anmonteiro/nix-overlays/archive/98b6ee7.tar.gz;
+    sha256 = "12vqdghlz2v7ln28jmrnxy210zv5x21g8kw2s0dvszfmz0p2inkh";
+  };
+
+  pkgs = import <nixpkgs> {
+    overlays = [
+      (import overlays)
+      (self: super: {
+        ocamlPackages = super.ocaml-ng."ocamlPackages_${ocamlVersion}".overrideScope'
+            (super.callPackage "${overlays}/ocaml" {});
+      })
+    ];
+  };
+
+in
+  pkgs
